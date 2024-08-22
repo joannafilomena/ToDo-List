5285c9a94d134029197635eb10a09bc88c5a0931
Joanna Kaminska <joannafilomena@gmail.com>
2024-08-22 22:33:01 +0200

    remove tasks

--- js/scirpt.js
+++ js/scirpt.js
@@ -9,17 +9,7 @@
             content: "zjeść pierogi",
             done: true,
         },
-    ]
-
-    const render = () => {
-        let htmlString = "";
-
-        for (const task of tasks) {
-            htmlString += `<li ${task.done ? "style=\"text-decoration:line-through\"" : ""}>${task.content}</li>`
-        }
-
-        document.querySelector(".js-tasks").innerHTML = htmlString;
-    };
+    ];
 
     const addNewTask = (newTaskContent) => {
         tasks.push({
@@ -29,8 +19,30 @@
         render();
     };
 
-const onFormSubmit = (event) => {
-    
+    function removeTask(index) {
+        tasks.splice(index, 1);
+        render();
+    }
+
+    const render = () => {
+        let htmlString = "";
+
+        for (const task of tasks) {
+            htmlString += `<li ${task.done ? "style=\"text-decoration:line-through\"" : ""}><button class="js-remove">Usuń</button>${task.content}</li>`
+        }
+
+        document.querySelector(".js-tasks").innerHTML = htmlString;
+
+        const removeButtons = document.querySelectorAll(".js-remove");
+        removeButtons.forEach((removeButton, index) => {
+            removeButton.addEventListener("click", () => {
+                removeTask(index);
+            })
+        })
+    };
+
+    const onFormSubmit = (event) => {
+
         event.preventDefault();
 
         const newTaskContent = document.querySelector(".js-newTask").value.trim();
@@ -39,7 +51,7 @@ const onFormSubmit = (event) => {
         }
 
         addNewTask(newTaskContent);
-};
+    };
 
     const init = () => {
         render();
